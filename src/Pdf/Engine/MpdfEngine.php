bd0c87059aa3098f2dd88dbe63d6aca27c600fc7
androideo <oli1925@hotmail.com>
2020-08-19 09:33:50 +0200

    Update MpdfEngine.php

--- src/Pdf/Engine/MpdfEngine.php
+++ src/Pdf/Engine/MpdfEngine.php
@@ -15,25 +15,25 @@ class MpdfEngine extends AbstractPdfEngine
      */
     public function output(): string
     {
-        $orientation = $this->_Pdf->orientation() === 'landscape' ? 'L' : 'P';
-        $format = $this->_Pdf->pageSize();
-        if (
-            is_string($format)
-            && $orientation === 'L'
-            && strpos($format, '-L') === false
-        ) {
-            $format .= '-' . $orientation;
-        }
-
-        $options = [
-            'mode' => $this->_Pdf->encoding(),
-            'format' => $format,
-            'orientation' => $orientation,
-            'tempDir' => TMP,
-        ];
-        $options = array_merge($options, (array)$this->getConfig('options'));
-
         $Mpdf = $this->_createInstance($options);
+        
+        if(!this->_Pdf->headerOnFirstPage())
+            $Mpf->AddPage('','');
+        
+        $header = $this->_Pdf->header();
+        foreach ($header as $location => $html) {
+            if ($html !== null) {
+                $Mpf->SetHTMLHeader($html);
+            }
+        }
+        
+        $footer = $this->_Pdf->footer();
+        foreach ($footer as $location => $html) {
+            if ($html !== null) {
+                $Mpf->SetHTMLFooter($html);
+            }
+        }
+        
         $Mpdf->WriteHTML($this->_Pdf->html());
 
         return $Mpdf->Output('', Destination::STRING_RETURN);
@@ -47,6 +47,38 @@ class MpdfEngine extends AbstractPdfEngine
      */
     protected function _createInstance($options): Mpdf
     {
+        $orientation = $this->_Pdf->orientation() === 'landscape' ? 'L' : 'P';
+        $format = $this->_Pdf->pageSize();
+        if (
+            is_string($format)
+            && $orientation === 'L'
+            && strpos($format, '-L') === false
+        ) {
+            $format .= '-' . $orientation;
+        }
+        
+        $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
+        $fontDirs = $defaultConfig['fontDir'];
+
+        $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
+        $fontData = $defaultFontConfig['fontdata'];
+
+        $options = [
+            'fontDir' => array_merge($fontDirs, [
+                $this->_Pdf->getCustomFontDir(),
+            ]),
+            'fontdata' => 
+                $fontData + 
+                $this->_Pdf->getCustomFontArray()
+            ],
+            'default_font' => array_key_first($this->_Pdf->getCustomFontArray()),
+            'mode' => $this->_Pdf->encoding(),
+            'format' => $format,
+            'orientation' => $orientation,
+            'tempDir' => TMP,
+        ];
+        $options = array_merge($options, (array)$this->getConfig('options'));
+                        
         return new Mpdf($options);
     }
 }
